adaf627b18ebe7dae95d7048fbac91757c09ee61
cassarino95 <alessio.cassarino@proactivity.it>
2023-01-12 12:57:48 +0100

    Created file hibernate.cfg.xml and persistence.xml and setting all the data for connecting the database gioco_del_lotto.
    Created Wheel class and mapping all the attribute except one to many waiting for creation of extraction class

---
+++ src/main/java/it/proactivity/model/Wheel.java
@@ -0,0 +1,40 @@
+package it.proactivity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wheel")
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Setter
+public class Wheel {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    /*
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="wheel_id")
+    private List<Extraction> extractionList;
+
+     */
+
+    @Override
+    public String toString() {
+        return id + " " + city;
+    }
+}
