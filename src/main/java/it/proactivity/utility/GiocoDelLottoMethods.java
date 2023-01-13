74d8aebd3c349300e2b30e78d48015daf1c49fcd
cassarino95 <alessio.cassarino@proactivity.it>
2023-01-13 10:24:01 +0100

    open session in methods on GiocoDelLottoMethods class

--- src/main/java/it/proactivity/utility/GiocoDelLottoMethods.java
+++ src/main/java/it/proactivity/utility/GiocoDelLottoMethods.java
@@ -2,6 +2,9 @@ package it.proactivity.utility;
 
 import it.proactivity.model.Extraction;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import java.time.LocalDate;
@@ -10,14 +13,28 @@ import java.util.List;
 public class GiocoDelLottoMethods {
 
     public static List<Extraction> getAllExtractions(Session session) {
+
+        if(!session.isOpen()) {
+            session = createSession();
+            session.getTransaction();
+        }
+
         final String selectAllExtractions = "SELECT e FROM Extraction e";
         Query<Extraction> extractionQuery = session.createQuery(selectAllExtractions);
         List<Extraction> extractions = extractionQuery.list();
+
+        endSession(session);
 
         return extractions;
     }
 
     public static List<Extraction> getAllExtractionForOneDate(Session session, String date) {
+
+        if(!session.isOpen()) {
+            session = createSession();
+            session.getTransaction();
+        }
+
         if (date == null || date.isEmpty()) {
             return null;
         }
@@ -33,6 +50,20 @@ public class GiocoDelLottoMethods {
         Query<Extraction> extractionQuery = session.createQuery(getAllTransactionForOneDate)
                 .setParameter("extractionDate", parsedDate);
 
+        endSession(session);
+
         return extractionQuery.list();
+    }
+
+    private static Session createSession() {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        return session;
+    }
+    private static void endSession(Session session) {
+        session.getTransaction().commit();
+        session.close();
     }
 }
