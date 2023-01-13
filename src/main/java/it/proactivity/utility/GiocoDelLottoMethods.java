69b202678bee8717c07d6ce652de4c997c6e5987
cerrigham <cerringham@gmail.com>
2023-01-13 11:17:11 +0100

    added getAllExtractionsFromWheel methods, clean code, added test

--- src/main/java/it/proactivity/utility/GiocoDelLottoMethods.java
+++ src/main/java/it/proactivity/utility/GiocoDelLottoMethods.java
@@ -1,7 +1,10 @@
 package it.proactivity.utility;
 
 import it.proactivity.model.Extraction;
+import it.proactivity.model.Wheel;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import java.time.LocalDate;
@@ -9,21 +12,49 @@ import java.util.List;
 
 public class GiocoDelLottoMethods {
 
-    public static List<Extraction> getAllExtractions(Session session) {
+    public Session createSession() {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        return session;
+    }
+
+    private void checkSession(Session session) {
+        if (!session.isOpen()) {
+            session = createSession();
+        }
+        session.getTransaction();
+    }
+
+    public void endSession(Session session) {
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List<Extraction> getAllExtractions(Session session) {
+        checkSession(session);
+
         final String selectAllExtractions = "SELECT e FROM Extraction e";
         Query<Extraction> extractionQuery = session.createQuery(selectAllExtractions);
         List<Extraction> extractions = extractionQuery.list();
 
+        endSession(session);
+
         return extractions;
     }
 
-    public static List<Extraction> getAllExtractionForOneDate(Session session, String date) {
+    public List<Extraction> getAllExtractionForOneDate(Session session, String date) {
+        checkSession(session);
+
         if (date == null || date.isEmpty()) {
+            endSession(session);
             return null;
         }
 
         LocalDate parsedDate = ParsingUtility.parseStringToDate(date);
-        if(parsedDate == null) {
+        if (parsedDate == null) {
+            endSession(session);
             return null;
         }
 
@@ -33,6 +64,35 @@ public class GiocoDelLottoMethods {
         Query<Extraction> extractionQuery = session.createQuery(getAllTransactionForOneDate)
                 .setParameter("extractionDate", parsedDate);
 
-        return extractionQuery.list();
+        List<Extraction> extractions = extractionQuery.getResultList();
+
+        endSession(session);
+
+        return extractions;
     }
+
+    public List<Extraction> getAllExtractionsFromWheel(Session session, Wheel wheel) {
+        if (wheel == null) {
+            endSession(session);
+            return null;
+        }
+        final String selectAllExtractionsFromWheel = "SELECT e FROM Extraction e WHERE e.wheel = :wheel";
+        List<Extraction> extractions = session.createQuery(selectAllExtractionsFromWheel)
+                .setParameter("wheel", wheel).list();
+
+        endSession(session);
+        return extractions;
+    }
+
+    public Wheel getMilanoWheel(Session session) {
+        final String MILANO_CITY = "Milano";
+        final String milanoWheelQueryString = "SELECT w " +
+                "FROM Wheel w " +
+                "WHERE w.city = :milanoCity";
+        Query<Wheel> wheelQuery = session.createQuery(milanoWheelQueryString)
+                .setParameter("milanoCity", MILANO_CITY);
+        return wheelQuery.getSingleResult();
+    }
+
+
 }
--- src/test/java/it/proactivity/test/GiocoDelLottoMethodsTest.java
+++ src/test/java/it/proactivity/test/GiocoDelLottoMethodsTest.java
@@ -1,10 +1,10 @@
 package it.proactivity.test;
 
 import it.proactivity.model.Extraction;
+import it.proactivity.model.Wheel;
 import it.proactivity.utility.GiocoDelLottoMethods;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +14,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GiocoDelLottoMethodsTest {
+    private GiocoDelLottoMethods getGiocoDelLottoMethods() {
+        return new GiocoDelLottoMethods();
+    }
 
     private Session createSession() {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
@@ -22,28 +25,12 @@ class GiocoDelLottoMethodsTest {
         return session;
     }
 
-    private void closeSession(Session session) {
-        session.close();
-    }
-
-    private static Transaction beginTransaction(Session session) {
-        return session.getTransaction();
-    }
-
-    private static void endTransaction(Session session) {
-        session.getTransaction().commit();
-    }
-
     @Test
     public void getAllExtractionsPositiveTest() {
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractionList = getGiocoDelLottoMethods().getAllExtractions(session);
 
-        List<Extraction> extractionList = GiocoDelLottoMethods.getAllExtractions(session);
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
         assertNotNull(extractionList);
         assertTrue(extractionList.size() > 0);
     }
@@ -51,13 +38,9 @@ class GiocoDelLottoMethodsTest {
     @Test
     public void getAllExtractionForOneDatePositiveTest() {
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractions = getGiocoDelLottoMethods().getAllExtractionForOneDate(session,"2023-01-12");
 
-        List<Extraction> extractions = GiocoDelLottoMethods.getAllExtractionForOneDate(session,"2023-01-12");
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
         assertNotNull(extractions);
         assertTrue(extractions.size() > 0);
         extractions.stream()
@@ -66,35 +49,28 @@ class GiocoDelLottoMethodsTest {
                                 !(e.getFifthNumber().equals(null))))))));
 
     }
-
+    
     @Test
     public void checkExtractionListDatePositive() {
-
         LocalDate date = LocalDate.of(2023,01,12);
+
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractions = getGiocoDelLottoMethods().getAllExtractionForOneDate(session,"2023-01-12");
 
-        List<Extraction> extractions = GiocoDelLottoMethods.getAllExtractionForOneDate(session,"2023-01-12");
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
         extractions.stream()
                 .forEach(e -> assertTrue(e.getExtractionDate().equals(date)));
     }
 
     @Test
     public void getAllExtractionsNegativeTest() {
-
         LocalDate date = LocalDate.of(2023,01,14);
+
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractions = getGiocoDelLottoMethods().getAllExtractionForOneDate(session,"2023-01-12");
 
-        List<Extraction> extractions = GiocoDelLottoMethods.getAllExtractionForOneDate(session,"2023-01-12");
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
+        assertFalse(session.isOpen());
         extractions.stream()
                 .forEach(e -> assertFalse(e.getExtractionDate().equals(date)));
     }
@@ -102,39 +78,46 @@ class GiocoDelLottoMethodsTest {
     @Test
     public void getAllExtractionForOneDateNegativeNullDateTest() {
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractions = getGiocoDelLottoMethods().getAllExtractionForOneDate(session,null);
 
-        List<Extraction> extractions = GiocoDelLottoMethods.getAllExtractionForOneDate(session,null);
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
         assertNull(extractions);
     }
 
     @Test
     public void getAllExtractionForOneDateNegativeWrongFormatDateTest() {
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractions = getGiocoDelLottoMethods().getAllExtractionForOneDate(session,"12-01-2023");
 
-        List<Extraction> extractions = GiocoDelLottoMethods.getAllExtractionForOneDate(session,"12-01-2023");
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
         assertNull(extractions);
     }
 
     @Test
     public void getAllExtractionForOneDateNegativeEmptyDateTest() {
         Session session = createSession();
-        beginTransaction(session);
+        List<Extraction> extractions = getGiocoDelLottoMethods().getAllExtractionForOneDate(session,"");
 
-        List<Extraction> extractions = GiocoDelLottoMethods.getAllExtractionForOneDate(session,"");
-
-        endTransaction(session);
-        closeSession(session);
-
+        assertFalse(session.isOpen());
         assertNull(extractions);
+    }
+
+    @Test
+    public void getAllExtractionsFromAWheelPositiveTest() {
+        Session session = createSession();
+        Wheel milanoWheel = getGiocoDelLottoMethods().getMilanoWheel(session);
+        List<Extraction> extractionList = getGiocoDelLottoMethods().getAllExtractionsFromWheel(session, milanoWheel);
+
+        assertFalse(session.isOpen());
+        assertNotNull(extractionList != null);
+    }
+
+    @Test
+    public void getAllExtractionsFromAWheelNegativeTestNullWheel() {
+        Session session = createSession();
+        List<Extraction> extractionList = getGiocoDelLottoMethods().getAllExtractionsFromWheel(session, null);
+
+        assertFalse(session.isOpen());
+        assertNull(extractionList);
     }
 }
